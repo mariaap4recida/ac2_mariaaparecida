eecd85b02b2f5a11fb3abac3b81d5f0e4775997e
mariaap4recida <mellomash48@gmail.com>
2024-04-22 14:52:44 -0400

    aa

--- exe2/exe2.php
+++ exe2/exe2.php
@@ -21,19 +21,20 @@
     $idade = $data - $ano;
     if ($ano && $nome == true) {
         if ($idade > 1 && $idade < 11) {
-            echo "Olá $nome, voce tem $idade logo voce é criança...	&#8986;<br/>"; }
-        elseif ($idade > 12 && $idade < 14) {
-            echo "Olá $nome, voce tem $idade logo voce é adolecente...	&#8986;<br/>";
+            echo "Olá $nome, voce tem $idade logo voce é criança...	&#8986;<br/>"; 
         }
-        elseif ($idade > 15 && $idade < 18) {
-            echo "Olá $nome, voce tem $idade logo voce é jovem...	&#8986;<br/>";
-        }
-        elseif ($idade > 19 && $idade < 64) {
-            echo "Olá $nome, voce tem $idade logo voce adulto...	&#8986;<br/>";
-        }
-        else {
-            echo "Olá $nome, voce tem $idade logo voce é idoso...	&#8986;<br/>";
-        }
+            elseif ($idade > 12 && $idade < 14) {
+                echo "Olá $nome, voce tem $idade logo voce é adolecente...	&#8986;<br/>";
+            }
+                elseif ($idade > 15 && $idade < 18) {
+                    echo "Olá $nome, voce tem $idade logo voce é jovem...	&#8986;<br/>";
+                }
+                    elseif ($idade > 19 && $idade < 64) {
+                        echo "Olá $nome, voce tem $idade logo voce adulto...	&#8986;<br/>";
+                    }
+                        else {
+                            echo "Olá $nome, voce tem $idade logo voce é idoso...	&#8986;<br/>";
+                        }
         
     }
     else {
